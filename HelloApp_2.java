8d896ee05aafe626c3cfeb0d4ccf81acc2ee542d
omrajput-jsr <omrajputjsr@gmail.com>
2026-03-11 11:17:32 +0530

    Coded UC2: Display Hello with Command-Line Argument

---
+++ HelloApp_2.java
@@ -0,0 +1,11 @@
+/*
+*@author:Om Rajput
+*@version:1.0
+*/
+
+public class HelloApp_2{
+ public static void main(String[] args){
+    String name = args[0];
+	System.out.println("Hello, " + name + "!");
+	}
+}
